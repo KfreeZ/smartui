5461695e165ecac80518b58280ce68401db1f66c
root <root@localhost.localdomain>
2017-05-17 22:59:53 -0400

    051803

--- public/smart_relay.js
+++ public/smart_relay.js
@@ -1,45 +1,100 @@
 $(document).ready(function(){
-    $("#demo-navbar .dropdown-menu a").click(function(){
-        var href=$(this).attr("href");
+    var btn = document.getElementById('applyBtn');
+    var vender = document.getElementById('vender');
+    // var detail_scope1 = document.getElementById("detail1");
+    // console.log(detail_scope1);
+    console.log(vender);
+    var venderValue;
+    var deviceValue;
+    var modeValue;
 
-        $("#tab-list a[href='" + href + "']").tab("show")
+    $("#mode li").click(function () {
+        modeValue = $(this).text(); //获取点击li的值      
     });
+    $("#vender li").click(function () {
+        venderValue = $(this).text(); //获取点击li的值      
+    });
+    $("#device li").click(function () {
+        deviceValue = $(this).text(); //获取点击li的值      
+    });
+
+    btn.addEventListener('click', function(){
+    
+        var myData = {};
+        var scopes = {};
+        
+        myData['mode'] = modeValue;
+        myData['vender'] = venderValue;
+        myData['device'] = deviceValue;
+        console.log(myData);
+        var aj = $.ajax( {    
+        url:'apply',  
+        data:myData,
+        type:'post',    
+        cache:true,  
+        dataType:'json',    
+        success:function() {
+            console.log(data);
+            // ajax_refresh_occupancy            
+            //$.get("refresh",function(data){
+                 
+            //    $("#scope1_occupancy").innerHTML()
+            //});   
+        },    
+        error : function() {    
+            // view("异常！");    
+            alert("异常！");    
+        }    
+        });  
+    })
+
+    // detail_scope1
+    $("#detail1").click(function(){
+        $.get("url",function(data){
+            $("#detail1_info").innerHTML(data)
+        });
+    });
+
+ // // ajax_refresh_scope1_occupancy 定时局部刷新
+ //    $(function(){
+ //        setInterval(refresh_scope1,10);
+ //        function refresh_scope1(){
+ //            $.get("url",function(data){
+
+ //                $("#scope1_occupancy").innerHTML(data)
+ //        });
+ //     }
+
 });
 
 
 
 
-// $(function(){
-// 	var data = [
-// 	                {name : 'IE',value : 35.75,color:'#a5c2d5'},
-// 		            {name : 'Chrome',value : 29.84,color:'#cbab4f'},
-// 		            {name : 'Firefox',value : 24.88,color:'#76a871'},
-// 		            {name : 'Safari',value : 6.77,color:'#9f7961'},
-// 		            {name : 'Opera',value : 2.02,color:'#a56f8f'},
-// 		            {name : 'Other',value : 0.73,color:'#6f83a5'}
-// 		    ];
-        	
-// 	new iChart.Bar2D({
-// 		    render : 'canvasDiv',
-// 			data: data,
-// 			title : 'Top 5 Browsers from 1 to 29 Feb 2012',
-// 			showpercent:true,
-// 			decimalsnum:2,
-// 			width : 800,
-// 			height : 400,
-// 			coordinate:{
-// 				scale:[{
-// 						 position:'bottom',	
-// 						 start_scale:0,
-// 						 end_scale:40,
-// 						 scale_space:8,
-// 						 listeners:{
-// 							parseText:function(t,x,y){
-// 								return {text:t+"%"}
-// 							}
-// 						}
-//                     }]
-//                 }
-// 			}).draw();
-// 	});
-	
+
+
+  //           $.ajax({
+  //                 url: `demo0.php?name=${username}`,
+  //                 dataType: 'json',
+  //                 method: 'GET',
+  //                 success: function(data) {
+  //                    if (data.result == 1) {
+  //                        $('.box').html(`<div>你的姓名${username}已成功保存。</div>`);
+  //                    }
+  //                },
+  //                error: function(xhr) {
+  //                    // 导致出错的原因较多，以后再研究
+  //                    alert('error:' + JSON.stringify(xhr));
+  //                }
+  //            })
+  //            .done(function(data) {
+
+  //                console.log('success');
+  //            })
+  //            .fail(function() {
+  //                console.log('error');
+  //            })
+  //            .always(function() {
+  //                console.log('complete');
+  //            });
+
+  // 
